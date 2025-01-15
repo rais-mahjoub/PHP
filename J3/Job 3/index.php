ea74f0ff921ade3ebed22cb84492b3b262cb7070
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-01-15 10:26:09 +0100

    feat: J3/Job 3

---
+++ J3/Job 3/index.php
@@ -0,0 +1,8 @@
+<?php
+$str = "I'm sorry Dave I'm afraid I can't do that.";
+$voyelles = array("a","e","i","o","u","y","A","E","I","O","U","Y") ;
+for ($i = 0; isset($str[$i]); $i++) {
+    if (in_array($str[ $i ], $voyelles)) {
+        echo $str[$i];
+    }
+}
