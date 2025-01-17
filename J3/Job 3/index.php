1ade97d57b23c4fc943f46bcfa57a762ff4f3957
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-01-17 09:53:48 +0100

    fix: J3/Job 3

--- J3/Job 3/index.php
+++ J3/Job 3/index.php
@@ -2,7 +2,9 @@
 $str = "I'm sorry Dave I'm afraid I can't do that.";
 $voyelles = array("a","e","i","o","u","y","A","E","I","O","U","Y") ;
 for ($i = 0; isset($str[$i]); $i++) {
-    if (in_array($str[ $i ], $voyelles)) {
-        echo $str[$i];
+    for ($j = 0; isset($voyelles[$j]); $j++) {
+        if ($str[ $i ] == $voyelles[$j]) {
+            echo $str[$i];
+        }
     }
 }
