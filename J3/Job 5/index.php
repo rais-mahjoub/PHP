527c83882b7035551a57f73166ea08226e4f928b
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-01-17 09:57:54 +0100

    fix: J3/Job 5

--- J3/Job 5/index.php
+++ J3/Job 5/index.php
@@ -4,10 +4,15 @@ $cons = ["z","r","t","p","q","s","d","f","g","h","j","k","l","m","w","x","c","v"
 $voy = ["a","e","i","o","u","y","A","E","I","O","U","Y"];
 $dic = ["Consonnes" => 0,"Voyelles" => 0];
 for ($i = 0; isset($str[$i]); $i++) {
-    if (in_array($str[ $i], $cons)) {
-        $dic['Consonnes'] ++;
-    } elseif (in_array($str[ $i], $voy)) {
-        $dic['Voyelles'] ++;
+    for ($j = 0; isset($cons[$j]); $j++) {
+        if ($str[ $i] == $cons[$j]) {
+            $dic['Consonnes'] ++;
+        }
+    }
+    for ($j = 0; isset($voy[$j]); $j++) {
+        if ($str[ $i] == $voy[$j]) {
+            $dic['Voyelles'] ++;
+        }
     }
 }
 ?>
