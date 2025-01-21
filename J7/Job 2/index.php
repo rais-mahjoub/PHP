2e5dcc64efa992a75b9adc97fd0e1b44bf7d28a3
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-01-21 12:35:58 +0100

    feat: J7/Job 2

---
+++ J7/Job 2/index.php
@@ -0,0 +1,23 @@
+<?php
+setcookie("nbvisites",1,time()+3600);
+
+if(!isset($_COOKIE["nbvisites"])) {
+    $vis=1;
+} else {
+    $vis = $_COOKIE["nbvisites"]+1;
+}
+setcookie("nbvisites",$vis,time()+3600);
+
+
+
+if(isset($_POST["reset"])) {
+    setcookie("nbvisites",0);
+}
+
+
+?>
+
+<form action="index.php" method="post">
+    <label><?=$vis?><br></label>
+    <input type="submit" value="reset" name="reset">
+</form>
---
+++ J7/Job 3/index.php
@@ -0,0 +1,10 @@
+<?php
+/*foreach (nombre del cookie as $k => $v) {
+    echo $v;
+}*/
+?>
+<form action="index.php" method="post">
+    <label for="list">Prénom :</label><br>
+    <input type="text" name="list"><br>
+    <input type="submit">
+</form>
