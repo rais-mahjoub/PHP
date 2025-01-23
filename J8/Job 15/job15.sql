b20d821bb552d2910ec2cca88423ecfbe51dd4c9
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-01-23 15:01:15 +0100

    fix: J8/Job 15

--- J8/Job 15/job15.sql
+++ J8/Job 15/job15.sql
@@ -1,2 +1,1 @@
-SELECT salles.Nom AS nom_salle, etage.Nom AS nom_etage 
-FROM salles JOIN etage ON salles.id_etage = etage.ID;
+SELECT salles.Nom, etage.Nom FROM salles JOIN etage ON salles.id_etage = etage.ID;
